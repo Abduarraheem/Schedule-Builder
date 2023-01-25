9c397d98f4472245acb50e001a83ce3f201eef69
Wazyre <32106559+Wazyre@users.noreply.github.com>
2023-01-24 17:40:38 -0800

    Setup server and ENV file for backend

---
+++ server.js
@@ -0,0 +1,45 @@
+import path from 'path'
+import dotenv  from 'dotenv'
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import passport from 'passport'
+
+dotenv.config();
+
+//Start Express and define local path
+const app = express();
+const __dirname = path.resolve();
+
+const urlencodedParser = express.urlencoded({extended: false});
+app.use(urlencodedParser, express.json());
+
+app.use(passport.initialize());
+
+//Env variables
+const PORT = process.env.PORT || 5000;
+
+//Setup APIs
+
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
+//Connect to database
+mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true})
+.then((res) => {
+  console.log("Database connected");
+})
+.catch((err) => console.log(err));
+
+//Log if server is up
+app.listen(PORT, () => {
+  console.log("App is running in", process.env.NODE_ENV, "mode on port", PORT);
+});
