04a0430876a0807c376147deefa18da776d087e7
Wazyre <32106559+Wazyre@users.noreply.github.com>
2023-01-24 23:25:33 -0800

    Add password de/encrypt to schedule add/update

--- backend/controllers/scheduleController.js
+++ backend/controllers/scheduleController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import bcrypt from 'bcryptjs'
 import Schedule from '../models/scheduleModel.js'
 
 // Gets all schedules and sends them to client
@@ -28,28 +29,46 @@ export const getSchedule = asyncHandler(async(req, res) => {
 
 // Adds new schedule to collection
 export const addSchedule = asyncHandler(async(req, res) => {
-    // If title already exists, return error
-    await Schedule.findOne({title: req.body.title}).then(schedule => {
-        if(schedule) {
-          return res.status(400).json({titleFound: "Schedule with title already exists."});
-        }
-      });
-    
-    // Put data from request into new document
-    const newSchedule = new Schedule({
-      title: req.body.title,
-      password: req.body.password,
-      classes: req.body.classes,
-      credits: req.body.credits,
-      quarter: req.body.quarter,
-      events: req.body.events
-    });
+  // If title already exists, return error
+  await Schedule.findOne({title: req.body.title}).then(schedule => {
+    if(schedule) {
+      return res.status(400).json({titleFound: "Schedule with title already exists."});
+    }
+  });
   
-    // Save new schedule to MongoDB, and send it to frontend in res
+  // Put data from request into new document
+  const newSchedule = new Schedule({
+    title: req.body.title,
+    password: req.body.password,
+    classes: req.body.classes,
+    credits: req.body.credits,
+    quarter: req.body.quarter,
+    events: req.body.events
+  });
+
+  // Encrypt and hash the password if exists, and save to MongoDB and frontend in res
+  if(password !== "") {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newSchedule.password, salt, (err, hash) => {
+        if(err) throw err;
+        newSchedule.password = hash;
+        newSchedule
+          .save()
+          .then(schedule => {
+            res.json(schedule);
+          })
+          .catch(err => console.log(err));
+      })
+    })
+  }
+
+  // Save new schedule to MongoDB, and send it to frontend in res
+  else {
     newSchedule
-      .save()
-      .then(newItem => res.json(newItem))
-      .catch(err => console.log(err));
+    .save()
+    .then(newItem => res.json(newItem))
+    .catch(err => console.log(err));
+  }
 });
 
 // Updates existing schedule 
@@ -60,6 +79,18 @@ export const updateSchedule = asyncHandler(async(req, res) => {
       return res.status(400).json({titleNotFound: "No schedule with that title found."});
     }
   });
+
+  // Compare the input password with the found schedule password if password exists
+  if(schedule.password !== "") {
+    bcrypt.compare(password, schedule.password).then(isMatch => {
+      if(isMatch) {
+        pass;
+      }
+      else {
+        return res.status(400).json({passwordInvalid: "Password is incorrect"});
+      }
+    });
+  }
 
   // Finds, updates, and sends schedule to MongoDB and frontend in res
   await Schedule.findOneAndUpdate(
--- backend/server.js
+++ backend/server.js
@@ -3,28 +3,28 @@ import dotenv  from 'dotenv'
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
-import passport from 'passport'
 import scheduleRoutes from './routes/scheduleRoutes.js'
 
+// Allow referencing .env files
 dotenv.config();
 
-//Start Express and define local path
+// Start Express and define local path
 const app = express();
 const __dirname = path.resolve();
 
+// Ecnode express to send json over https packets
 const urlencodedParser = express.urlencoded({extended: false});
 app.use(urlencodedParser, express.json());
 
-app.use(passport.initialize());
-
-//Env variables
+// Env variables
 const PORT = process.env.SERVER_PORT || 5000;
 const STATUS = process.env.NODE_ENV
 const DB_URI = process.env.DB_URI
-//Setup APIs
+
+// Setup APIs
 app.use('/api/schedules', scheduleRoutes);
 
-//Serve static assets if in production
+// Serve static assets if in production
 if(STATUS === 'production') {
   //Set static folder
   app.use(express.static('frontend/build'));
@@ -34,14 +34,14 @@ if(STATUS === 'production') {
   })
 }
 
-//Connect to database
+// Connect to database
 mongoose.connect(DB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then((res) => {
   console.log("Database connected");
 })
 .catch((err) => console.log(err));
 
-//Log if server is up
+// Log if server is up
 app.listen(PORT, () => {
   console.log("App is running in", STATUS, "mode on port", PORT);
 });
